33723c44e2b6c73dac7c594de81c8cbc6d104f6d
srajma <srimahalingam@protonmail.com>
2026-02-21 03:35:37 +0000

    hopefully fix

---
+++ frontend/src/lib/editorDefaults.js
@@ -0,0 +1,23 @@
+export const DEFAULT_INDIC_IMPORT = {
+  kind: 'lib',
+  username: 'srajma',
+  name: 'indic',
+  path: '/srajma/lib/indic/alpha',
+  selected_version: 'alpha',
+  _draft_version: 'alpha',
+  alias: 'indic',
+  filter_not: [],
+};
+
+export const DEFAULT_INDIC_IMPORT_CODE = 'indic = xatrahub("/srajma/lib/indic/alpha")\n';
+
+export const createDefaultBuilderOptions = () => ({
+  basemaps: [{ url_or_provider: 'Esri.WorldTopoMap', default: true }],
+  flag_color_sequences: [{ class_name: '', colors: '', step_h: 1.6180339887, step_s: 0.0, step_l: 0.0 }],
+  admin_color_sequences: [{ colors: '', step_h: 1.6180339887, step_s: 0.0, step_l: 0.0 }],
+  data_colormap: { type: 'LinearSegmented', colors: 'yellow,orange,red' },
+});
+
+export const createDefaultBuilderElements = () => ([
+  { type: 'flag', label: 'India', value: [{ op: 'union', type: 'gadm', value: 'IND' }], args: { note: 'Republic of India' } },
+]);
