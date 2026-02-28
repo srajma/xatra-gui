d94532caaa74ee20a9391d1ecf0d56b1136a8272
srajma <srimahalingam@protonmail.com>
2026-02-28 03:28:59 +0000

    swat river

--- xatra_lib/map/rivers_silkrd.py
+++ xatra_lib/map/rivers_silkrd.py
@@ -1,4 +1,5 @@
 xatra.River(label="Kubha (kabul)", value=overpass("1676476"), show_label=True)
+xatra.River(label="Suvāstu (Swat)", value=overpass("9612817"), show_label=True)
 xatra.River(label="Kama (kunar)", value=overpass("6608825"), show_label=True)
 xatra.River(label="Haraxvati (arghandab)", value=overpass("8623883"), show_label=True)
 xatra.River(label="Haetumant (helmand)", value=overpass("5252846"), show_label=True)
